824a01da9fb332f5e3073c93592d7719457842a8
SrRecursive <rojohnibana@gmail.com>
2023-10-06 23:30:59 +0200

    Fix typo

--- src/dll.c
+++ src/dll.c
@@ -3,15 +3,15 @@
 
 int	main(void)
 {
-	int		lenghtbackwards;
-	int		lenghtafterwards;
+	int		lengthbackwards;
+	int		lengthafterwards;
 	t_dll	*doublylinkedlist;
 
-	printf("Introduce the lenght of your doubly linked list backwards: ");
-	scanf("%d", &lenghtbackwards);
-	printf("Introduce the lenght of your doubly linked list afterwards: ");
-	scanf("%d", &lenghtafterwards);
-	doublylinkedlist = doubly_linked_list_generator(lenghtbackwards, lenghtafterwards);
+	printf("Introduce the length of your doubly linked list backwards: ");
+	scanf("%d", &lengthbackwards);
+	printf("Introduce the length of your doubly linked list afterwards: ");
+	scanf("%d", &lengthafterwards);
+	doublylinkedlist = doubly_linked_list_generator(lengthbackwards, lengthafterwards);
 	if (doublylinkedlist == NULL)
 		return (1);
 	destroy_doubly_linked_list(doublylinkedlist);
